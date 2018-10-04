67dea993cf18291f92e749045c333a40b1dad1bf
Ruslan <sokolruslan92@gmail.com>
2018-10-05 00:19:31 +0300

    Add files via upload

---
+++ precache-manifest.90cd5b710e187f97db6f4d9791418181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "445505eae0bac3e2aa37",
+    "url": "/bro-table-react/static/js/runtime~main.445505ea.js"
+  },
+  {
+    "revision": "35b6ae1ad6933d4f8618",
+    "url": "/bro-table-react/static/js/main.35b6ae1a.chunk.js"
+  },
+  {
+    "revision": "bda1ed9a35e36d70acfb",
+    "url": "/bro-table-react/static/js/1.bda1ed9a.chunk.js"
+  },
+  {
+    "revision": "35b6ae1ad6933d4f8618",
+    "url": "/bro-table-react/static/css/main.465cdc36.chunk.css"
+  },
+  {
+    "revision": "04c3e436d1d3cae88f8ec3de89cb85b4",
+    "url": "/bro-table-react/index.html"
+  }
+];
